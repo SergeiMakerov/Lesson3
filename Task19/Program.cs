88b3230caaf793c13b0b7dad0ef0623684b5e148
Sergei <makerovs@yandex.ru>
2023-01-24 11:55:31 +0700

    Tack 19

---
+++ Task19/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход 
+пятизначное число и проверяет, 
+является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.Write("Введите число: ");
+string? num = Console.ReadLine();
+
+void CheckNum(string num)
+{
+    if (num[0] == num[4] || num[1] == num[3])
+    {
+        Console.WriteLine($"Ваше число: {num} - палиндром.");
+    }
+    else Console.WriteLine($"Ваше число: {num} - НЕ палиндром.");
+}
+if (num!.Length == 5)
+{
+    CheckNum(num);
+}
+else Console.WriteLine($"Введи правильное число");
